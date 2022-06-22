b252a69f12d9a2abf1933de4a506cdcd155c43e5
Monty <Montspy@gmail.com>
2022-06-22 03:35:04 -0700

    Update bookmarket-list-cids-in-folder.js

--- bookmarket-list-cids-in-folder.js
+++ bookmarket-list-cids-in-folder.js
@@ -1,7 +1,7 @@
 // Bookmarklet
-function() {
+(function() {
 	for (var l = document.links, re = new RegExp("/ipfs/(Qm.{44})?"), i = 0; i < l.length; i++) l[i].classList.contains("ipfs-hash") && (match = l[i].href.match(re), match && (l[i].innerHTML = match[1], console.log(match[1])));
-}
+})();
 
 // Minified (copy-paste this)
 javascript:(function(){for(var l=document.links,re=new RegExp("/ipfs/(Qm.{44})?"),i=0;i<l.length;i++)l[i].classList.contains("ipfs-hash")&&(match=l[i].href.match(re),match&&(l[i].innerHTML=match[1],console.log(match[1])));})();
